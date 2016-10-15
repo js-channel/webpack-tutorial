dd94e72f865c3d5853126bec020d736f8f3af66e
Danilo <danilobjr@gmail.com>
2016-10-15 16:50:03 -0300

    webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+module.exports = {
+    entry: path.resolve(__dirname, 'src/js/app.js'),
+    output: {
+        path: path.resolve('build'),
+        filename: 'bundle.js'
+    }
+}
