66bd0809643ad89f18adf6bf876f0955b27474dd
Danilo <danilobjr@gmail.com>
2016-10-15 18:02:26 -0300

    applying HMR on javascript

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,12 @@
 const { sayHelloTo } = require('./hello.js');
+const hot = module.hot;
 
 const span = document.createElement('span');
 span.innerText = sayHelloTo('Javascript Developer');
 
 document.body.appendChild(span);
+
+if (hot) {
+    hot.accept();
+    hot.dispose(() => span.parentNode.removeChild(span));
+}
